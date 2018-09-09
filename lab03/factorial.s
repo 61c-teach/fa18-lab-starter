8c02d5f056b13ad3c7a62ec64ddefa31e79a0ffa
hershg <hersh.godse@gmail.com>
2018-09-08 23:13:33 -0700

    lab3 starter

---
+++ lab03/factorial.s
@@ -0,0 +1,18 @@
+.data
+n: .word 8
+
+.text
+main:
+    la t0, n
+    lw a0, 0(t0)
+    jal ra, factorial
+    
+    addi a1, a0, 0
+    addi a0, x0, 1
+    ecall # Print Result
+    
+    addi a0, x0, 10
+    ecall # Exit
+
+factorial:
+    # YOUR CODE HERE
